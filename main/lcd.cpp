fa7142e4dd77a56d87aceae95e32c1776915886d
andrea-covre <andrea.covre@icloud.com>
2022-04-14 01:14:54 -0400

    added dim-down feature

--- main/lcd.cpp
+++ main/lcd.cpp
@@ -16,6 +16,7 @@ class Lcd {
     const int D5 = 5;
     const int D6 = 6;
     const int D7 = 7;
+    const int LED = 8;
 
     // Screen properties
     const int width = 16;
@@ -26,9 +27,14 @@ class Lcd {
     Rtc rtc;
 
     // Animations variables
-    int const bootup_view_time = 5;
-    uint32_t state_switch_ts = 0; 
-    uint32_t ts;
+    int const bootup_view_time = 1;     // s
+    int const dim_down_time = 5;        // s
+    int const dim_down_step_time = 20;  // ms
+    int brightness = 255;
+    uint32_t led_dim_ts;                // s
+    uint32_t state_switch_ts;           // s
+    uint32_t view_update_ts;            // s
+    uint32_t ts;                        // s
 
     // Special chars struct
     struct specialChar {
@@ -64,6 +70,8 @@ class Lcd {
       // Set up screen
       lcd = new LiquidCrystal(RS, E, D4, D5, D6, D7);
       lcd->begin(width, height);
+      pinMode(LED, OUTPUT);
+      digitalWrite(LED, HIGH);
 
       // Upload special chars
       lcd->createChar(flower.index, flower.charMap);
@@ -77,6 +85,7 @@ class Lcd {
       rtc = rtc_obj;
       ts = rtc.get_ts();
       state_switch_ts = ts;
+      view_update_ts = ts;
     }
 
     State get_state() {
@@ -112,7 +121,6 @@ class Lcd {
 
     void default_led_on_view() {
       lcd->clear();
-      Serial.println("ccc");
       print_time();
       print_temp();
       print_hum();
@@ -121,26 +129,49 @@ class Lcd {
     
   // State machine
   public:void update() {
+    // Updating values
+    ts = rtc.get_ts();
+    
     switch(state) {
       case RESET:
         bootup_view();
         state = BOOTUP;
-        state_switch_ts = rtc.get_ts();
+        state_switch_ts = ts;
         break;
         
       case BOOTUP:
-        if (rtc.get_ts() - state_switch_ts > bootup_view_time) {
+        if (ts - state_switch_ts > bootup_view_time) {
           state = DEF_LED_ON;
-          state_switch_ts = rtc.get_ts();
+          state_switch_ts = ts;
         }
         break;
 
       case DEF_LED_ON:
-        if (rtc.get_ts() > state_switch_ts) {
+        if (ts > view_update_ts) {
           default_led_on_view();
-          state_switch_ts = rtc.get_ts();
+          view_update_ts = ts;
+        }
+
+        if (ts - state_switch_ts > dim_down_time) {
+          state = DEF_LED_OFF;
+          state_switch_ts = ts;
+          led_dim_ts = millis();
         }
         break;
+
+      case DEF_LED_OFF:
+        if (ts > view_update_ts) {
+          default_led_on_view();
+          view_update_ts = ts;
+        }
+        if (millis() - led_dim_ts > dim_down_step_time and brightness > 0) {
+          brightness -= 1;
+          analogWrite(LED, brightness);
+          led_dim_ts = millis();
+        }
+
+        break;
+        
     }
   }
 };
